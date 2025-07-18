19b855499b2214c24806077033f117e92fcfc370
mike1br <michael.braswell@hgs.hiddengeniusproject.org>
2025-07-18 08:53:47 -0700

    Update student_grades_analyzer.py

--- student_grades_analyzer.py
+++ student_grades_analyzer.py
@@ -1,6 +1,17 @@
-grades = [70,88,92,65,99]
-def print_grades(average_grade):
+grades = [88, 76, 93, 65, 81]
+def print_grades():
     for grade in grades:
-        print(grades)
-
-    def average_grades():
+        print(grade)
+def average_grade():
+    total = sum(grades)
+    count = len(grades)
+    return total / count
+def highest_grade():
+    return max(grades)
+def grade_feedback():
+    if grades >= 90:
+        return "Excellent"
+    elif grades >= 70 and grades < 90:
+        return "Good"
+    else:
+        return "Needs Improvement"
